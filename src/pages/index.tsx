c73290192a88d7f938543b620999ad45939d3d87
na0nie <naonchun@gmail.com>
2024-06-04 12:55:27 -0700

    mapbox token

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -158,6 +158,10 @@ const Home: NextPage = () => {
   const [infoBoxLength, setInfoBoxLength] = useState(1);
   const [arrestInfo, setArrestInfo] = useState(0);
   const [normalizeIntensity, setNormalizeIntensity] = useState(false);
+  const [mapboxConfig, setMapboxConfig] = useState<{
+    mapboxToken: string;
+    mapboxStyle: string;
+  } | null>(null);
 
   useEffect(() => {
     console.log("arrestData updated:", arrestData);
@@ -300,8 +304,23 @@ const Home: NextPage = () => {
   }, [normalizeIntensity]);
 
   useEffect(() => {
-    mapboxgl.accessToken =
-      "pk.eyJ1Ijoia2VubmV0aG1lamlhIiwiYSI6ImNsZG1oYnpxNDA2aTQzb2tkYXU2ZWc1b3UifQ.PxO_XgMo13klJ3mQw1QxlQ";
+    const fetchMapboxConfig = async () => {
+      try {
+        const response = await fetch("/api/mapboxConfig");
+        const data = await response.json();
+        setMapboxConfig(data);
+      } catch (error) {
+        console.error("Error fetching Mapbox config:", error);
+      }
+    };
+
+    fetchMapboxConfig();
+  }, []);
+
+  useEffect(() => {
+    if (mapboxConfig && divRef.current) {
+      mapboxgl.accessToken = mapboxConfig.mapboxToken;
+
 
     const formulaForZoom = () => {
       if (typeof window != "undefined") {
@@ -323,7 +342,7 @@ const Home: NextPage = () => {
 
     var mapparams: any = {
       container: divRef.current, // container ID
-      style: "mapbox://styles/kennethmejia/cln1vl9n1004i01pm3jni4fo4", // style URL (THIS IS STREET VIEW)
+      style: mapboxConfig.mapboxStyle,
       center: [-118.41, 34], // starting position [lng, lat]
       zoom: formulaForZoom(), // starting zoom
     };
@@ -919,7 +938,7 @@ const Home: NextPage = () => {
     if (getmapboxlogo) {
       getmapboxlogo.remove();
     }
-  }, []);
+  }}, [mapboxConfig]);
 
   useEffect(() => {
     let arrayoffilterables: any = [];
